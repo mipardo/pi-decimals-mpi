616118e4b57927d188c0c407a287b04723942694
mipardo <mipardo@uji.es>
2023-06-23 09:46:55 +0200

    refactor: removed GMP-SME-BLC-CYC (slow and with errors)

--- sources/gmp/algorithms/chudnovsky_simplified_expression_cheater_blocks.h
+++
@@ -1,7 +0,0 @@
-#ifndef GMP_CHUDNOVSKY_SIMPLIFIED_EXPRESSION_CHEATER_BLOCKS
-#define GMP_CHUDNOVSKY_SIMPLIFIED_EXPRESSION_CHEATER_BLOCKS
-
-void gmp_chudnovsky_simplified_expression_cheater_blocks_algorithm(int, int, mpf_t, int, int);
-
-#endif
-
